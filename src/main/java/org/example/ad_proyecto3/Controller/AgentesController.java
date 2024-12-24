f23b49a2832b2979448245393c708857b04f649e
bestiadivina147 <diegopc147@gmail.com>
2024-12-24 11:56:55 +0100

    feat(AgentesController.java): :sparkles: add AgentesController class

---
+++ src/main/java/org/example/ad_proyecto3/Controller/AgentesController.java
@@ -0,0 +1,38 @@
+package org.example.ad_proyecto3.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class AgentesController {
+
+    @FXML
+    private Button btnBuscarAgente;
+
+    @FXML
+    private ComboBox<?> cmbAgente;
+
+    @FXML
+    private Label lblAgente;
+
+    @FXML
+    private Label lblDescripcionAgente;
+
+    @FXML
+    private Label lblDescripcionBuscadoAgente;
+
+    @FXML
+    private Label lblIdAgente;
+
+    @FXML
+    private Label lblIdBuscadoAgente;
+
+    @FXML
+    private Label lblRolBuscadoAgente;
+
+    @FXML
+    private AnchorPane panelAgente;
+
+}
